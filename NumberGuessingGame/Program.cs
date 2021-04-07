77cc6d31a85aaaa719e0840e5fa845815041e0df
James Bushek <jcbushek@gmail.com>
2021-04-07 11:20:33 -0600

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"args[{i}]={args[i]}");
+            }
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"");
+            }*/
+
+            if (args.Length != 1)
+            {
+                throw new Exception("Please only input one number");
+            }
+
+            int usrGuess;
+            if (Int32.TryParse(args[0], out usrGuess))
+            {
+                var hail = new Hailstone(usrGuess);
+            }
+            else
+                throw new Exception("Please input a number, not whatever you just gave me");
+
+        }
+    }
+}
